0c795244afe3a8e3c123bb7d4dd2382203a5273c
Saluki <saluki@gmx.fr>
2018-08-02 16:41:19 +0200

    :recycle: Changed code to adapt to Winston 3

--- src/modules/common/provider/logger.service.ts
+++ src/modules/common/provider/logger.service.ts
@@ -1,20 +1,29 @@
 
-import { Logger, LoggerInstance, transports } from 'winston';
+import * as winston from 'winston';
 
 export class LoggerService {
 
-    private readonly instance: LoggerInstance;
+    private readonly instance: winston.Logger;
 
     public constructor() {
 
-        this.instance = new Logger({
+        const format = this.isProductionEnv() ?
+            winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json()
+            ) :
+            winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            );
+
+        this.instance = winston.createLogger({
+            level: 'info',
+            silent: this.isTestEnv(),
+            format,
             transports: [
-                new transports.Console({
-                    level: 'info',
-                    silent: (process.env.NODE_ENV === 'testing'),
-                    colorize: (process.env.NODE_ENV !== 'production'),
-                    timestamp: true,
-                    showLevel: true
+                new winston.transports.Console({
+                    stderrLevels: ['error']
                 })
             ]
         });
@@ -26,6 +35,14 @@ export class LoggerService {
 
     public error(message: string) {
         this.instance.error(message);
+    }
+
+    private isTestEnv(): boolean {
+        return process.env.NODE_ENV === 'test';
+    }
+
+    private isProductionEnv(): boolean {
+        return process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging';
     }
 
 }
