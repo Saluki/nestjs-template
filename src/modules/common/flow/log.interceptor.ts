a2fd0bdfdc8db721dce30075e2ee7ee194b67f3e
Saluki <saluki@gmx.fr>
2021-02-25 22:55:14 +0100

    Fixing log duration bug

--- src/modules/common/flow/log.interceptor.ts
+++ src/modules/common/flow/log.interceptor.ts
@@ -14,26 +14,26 @@ export class LogInterceptor implements NestInterceptor {
 
     public intercept(context: ExecutionContext, next: CallHandler): Observable<Response> {
 
-        const startTime = new Date().getMilliseconds();
+        const startTime = new Date().getTime();
         const request = context.switchToHttp().getRequest();
 
         return next.handle().pipe(
             map(data => {
                 const responseStatus = (request.method === 'POST') ? HttpStatus.CREATED : HttpStatus.OK;
-                this.logger.info(`${this.getTimeDelta(startTime)} ${request.ip} ${responseStatus} ${request.method} ${this.getUrl(request)}`);
+                this.logger.info(`${this.getTimeDelta(startTime)}ms ${request.ip} ${responseStatus} ${request.method} ${this.getUrl(request)}`);
                 return data;
             }),
             catchError(err => {
                 // Log fomat inspired by the Squid docs
                 // See https://docs.trafficserver.apache.org/en/6.1.x/admin-guide/monitoring/logging/log-formats.en.html
-                this.logger.error(`${this.getTimeDelta(startTime)} ${request.ip} ${err.status} ${request.method} ${this.getUrl(request)}`);
+                this.logger.error(`${this.getTimeDelta(startTime)}ms ${request.ip} ${err.status} ${request.method} ${this.getUrl(request)}`);
                 return throwError(err);
             })
         );
     }
 
     private getTimeDelta(startTime: number): number {
-        return new Date().getMilliseconds() - startTime;
+        return new Date().getTime() - startTime;
     }
 
     private getUrl(request: Request): string {
